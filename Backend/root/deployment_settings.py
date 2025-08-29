cb963986856df0da375ee0defda5e61edf259fea
ManojKumark12 <mk3804181@gmail.com>
2025-08-29 11:36:01 +0530

    added vercel

--- Backend/root/deployment_settings.py
+++ Backend/root/deployment_settings.py
@@ -46,23 +46,4 @@ DATABASES = {
     )
 }
 
-# ✅ Logging
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "mail_admins": {
-            "level": "ERROR",
-            "class": "django.utils.log.AdminEmailHandler",
-        },
-    },
-    "loggers": {
-        "django": {
-            "handlers": ["mail_admins"],
-            "level": "ERROR",
-            "propagate": True,
-        },
-    },
-}
-
 
