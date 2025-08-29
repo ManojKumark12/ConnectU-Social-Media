ec33d745a7c7d30e7b5c93de7608471804815163
ManojKumark12 <mk3804181@gmail.com>
2025-08-29 14:11:35 +0530

    fixurl function

--- frontend/src/components/firurl.js
+++ frontend/src/components/firurl.js
@@ -1,5 +1,5 @@
 export const fixUrl = (url) => {
-    let decoded=decodeURIComponent(feed.image).replace(/^\/+/, '').replace("https://connectu-social-media-jhi7.onrender.com/",'');
+    let decoded=decodeURIComponent(url).replace(/^\/+/, '').replace("https://connectu-social-media-jhi7.onrender.com/",'');
 
   // Ensure it has https:// not https:/
   if (decoded.startsWith("https:/") && !decoded.startsWith("https://")) {
